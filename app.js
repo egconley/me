f60063405ea9786070fe971c926251b072964afa
egconley <31393715+egconley@users.noreply.github.com>
2019-11-28 10:48:34 -0800

    added jQuery hover effect on menu li's in navbar for desktop layout

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 'use strict';
 
+// hover effect for menu items in desktop layout
+$(document).ready(function () {
+  $('li').hover(function () {
+    $(this).css('background-color', 'rgb(164, 208, 255)');
+    console.log('It happened!');
+  }, function () {
+    $(this).css('background-color', '');
+  });
+})
+
+// burger menu to X on click
 function burgerToX(x) {
   x.classList.toggle('changeMenu');
   $('#menuContainer').toggle();
 }
+
+
